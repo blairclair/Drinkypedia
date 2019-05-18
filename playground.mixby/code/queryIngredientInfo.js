9f6596211422d1453e3d8da5a845af715fb3f2ae
Ariella Grodzinsky <agrodzin@e1z1r4p35.42.us.org>
2019-05-18 12:34:13 -0700

    preparing to merge

--- playground.mixby/code/queryIngredientInfo.js
+++ playground.mixby/code/queryIngredientInfo.js
@@ -3,7 +3,6 @@ var console = require('console')
 var config = require('config')
 
 module.exports.function = function queryIngredientInfo (strIngredient1) {
-   var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i='
-   var results = http.getUrl(apiUrl + arg1)
-   return results
+   var Ingredients = http.getUrl('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=' + strIngredient1, { format: 'json' })
+   return Ingredients
 }
