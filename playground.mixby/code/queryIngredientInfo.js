38d19462e23aa5b95ccfa5804bb39d802dd8d874
Ariella Grodzinsky <agrodzin@e1z1r4p35.42.us.org>
2019-05-18 11:23:49 -0700

    added new classes

---
+++ playground.mixby/code/queryIngredientInfo.js
@@ -0,0 +1,9 @@
+var http = require('http')
+var console = require('console')
+var config = require('config')
+
+module.exports.function = function queryIngredientInfo (strIngredient1) {
+   var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=arg1'
+   var results = http.getUrl(apiUrl + arg1)
+   return results
+}
