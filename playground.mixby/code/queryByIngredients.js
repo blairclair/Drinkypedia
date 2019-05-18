7392c61a5a50a95f7a648246289436d89b30c408
Alexander Kozma <alexanders.kozma@gmail.com>
2019-05-18 12:50:58 -0700

    big functionality

--- playground.mixby/code/queryByIngredients.js
+++ playground.mixby/code/queryByIngredients.js
@@ -7,10 +7,11 @@ module.exports.function = function queryByIngredients (strIngredient1) {
   var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
   var results = []
   //var xhttp = new XMLHttpRequest();
-  for (var i = 0; i < arguments.length; i++) {
-    var tmpResults = http.getUrl(apiUrl + arguments[i])
+  for (var i = 0; i < arguments[0].length; i++) {
+    var tmpResults = http.getUrl(apiUrl + arguments[0][i])
     tmpResults = JSON.parse(tmpResults)
-    console.log(tmpResults.drinks)
+    console.log(apiUrl + arguments[0][i])
+    //console.log(tmpResults.drinks)
     if (typeof(results[0]) == 'undefined') {
       for (id in tmpResults.drinks) {
         console.log(tmpResults.drinks[id])
@@ -19,18 +20,21 @@ module.exports.function = function queryByIngredients (strIngredient1) {
     } else {
       var newResults = []
       for (id in tmpResults.drinks) {
-        console.log(apiUrl + arguments[i])
-        if (results.includes(tmpResults.drinks[id])) {
+        var found = 0
+        console.log(apiUrl + arguments[0][i])
+        for (var i = 0; i < results.length; i++) {
+          console.log(results[i].strDrink)
+          if (results[i].strDrink == tmpResults.drinks[id].strDrink) {
+            found += 1
+          }
+        }
+        if (found) {
           newResults.push(tmpResults.drinks[id])
         }
       }
       results = newResults;
     }
   }
-  //debug
-  /*for (id in results) {
-    console.log(results[id])
-  }*/
   console.log('Done')
   return results
 }
