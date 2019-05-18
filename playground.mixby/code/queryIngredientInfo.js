955ecf5a7bcc7f329c5cb2ed788357e401cecdd3
Alexander Kozma <alexanders.kozma@gmail.com>
2019-05-18 13:24:53 -0700

    training

--- playground.mixby/code/queryIngredientInfo.js
+++ playground.mixby/code/queryIngredientInfo.js
@@ -6,7 +6,7 @@ var console = require('console')
 module.exports.function = function queryIngredientInfo (strIngredient1) {
   var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i='
   var results = []
-  var tmpResults = http.getUrl(apiUrl + strIngredient1)
+  var tmpResults = http.getUrl(encodeURI(apiUrl + strIngredient1))
   tmpResults = JSON.parse(tmpResults)
   for (id in tmpResults.ingredients) {
     results.push(tmpResults.ingredients[id])
