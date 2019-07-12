8e77087d101f7f3a57173ec9f6782088f88d9af0
Ariella Grodzinsky <ariellagrodzinsky@gmail.com>
2019-07-12 11:33:06 -0700

    capitalizing ingredients

--- drinkyteam.drinkypedia/code/queryIngredientInfo.js
+++ drinkyteam.drinkypedia/code/queryIngredientInfo.js
@@ -9,6 +9,7 @@ module.exports.function = function queryIngredientInfo (strIngredient1) {
   var tmpResults = http.getUrl(encodeURI(apiUrl + strIngredient1))
   tmpResults = JSON.parse(tmpResults)
   for (id in tmpResults.ingredients) {
+    tmpResults.ingredients[id].strIngredient = tmpResults.ingredients[id].strIngredient.charAt(0).toUpperCase() + tmpResults.ingredients[id].strIngredient.substring(1)
     results.push(tmpResults.ingredients[id])
   }
   return results
